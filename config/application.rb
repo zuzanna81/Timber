0ec753761c13cf185a102ac6e8aea9ae08ecc426
Zuzanna <zuzanna.bienkowska@gmail.com>
2018-01-29 10:28:14 +0100

    Scaffold the rails app

---
+++ config/application.rb
@@ -0,0 +1,21 @@
+require_relative 'boot'
+
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+Bundler.require(*Rails.groups)
+
+module Timber
+  class Application < Rails::Application
+
+    config.load_defaults 5.1
+
+    config.generators.system_tests = nil
+  end
+end
