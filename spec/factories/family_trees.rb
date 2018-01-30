c5bb0e2010421f6727fddbeaec7dbf37ef8192cb
lisa_eriksson <lisa.c.eriksson@gmail.com>
2018-01-30 07:30:44 +0100

    Rf factory

--- spec/factories/family_trees.rb
+++ spec/factories/family_trees.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     relationship { Faker::HowIMetYourMother.character }
-    date_of_birth 2000-01-01
+    date_of_birth { Faker::Date.birthday(0, 120) }
     image { Faker::Avatar.image }
   end
 end
