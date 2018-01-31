cf4962f584e41f894e506bb8127e88078cad265a
Francesco <francesco.falchi@gmail.com>
2018-01-31 15:32:48 +0100

    Generate controller and model for photo

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,24 @@
+class PhotoAlbumsController < ApplicationController
+
+  def index
+    @photos = Photo.order('created_at')
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    if @photo.save
+      flash[:notice] = "Successfully created a new photo!"
+    else
+      flash[:alert] = "Error, no photo was created"
+      render :new
+    end
+  end
+
+  private
+  def photo_params
+    params.require(:photo).permit(:title, :image)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180129200207) do
+ActiveRecord::Schema.define(version: 20180131143139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,21 @@ ActiveRecord::Schema.define(version: 20180129200207) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+  end
+
+  create_table "photo_albums", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "attachment_file_name"
+    t.string "attachment_content_type"
+    t.integer "attachment_file_size"
+    t.datetime "attachment_updated_at"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
