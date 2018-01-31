2fe5777c5cc1bd21e6e2a21a644be5afd7fa5991
Francesco <francesco.falchi@gmail.com>
2018-01-31 22:11:25 +0100

    Move photo partial to partials folder, add has_attache_dimage to photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,8 @@
 class Photo < ApplicationRecord
-  has_attached_file :image
-  validates_attachment :image,
-                     content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+  # has_attached_file :image
+  # validates_attachment :image,
+  #                    content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+  include HasAttachedImage
 
   validates_presence_of :title
 end
