dbb49e73810ff11f0e12e673eb406a09646abff5
Francesco <francesco.falchi@gmail.com>
2018-01-31 23:08:06 +0100

    Add acceptance tests for navbar, all green

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -2,6 +2,22 @@ When("I visit index page") do
   visit root_path
 end
 
+When("I click on {string} link") do |link|
+  click_link link
+end
+
 Then("I should see {string}") do |content|
   expect(page).to have_content content
+end
+
+Then("the {string} should be visible") do |selector|
+  expect(page).to have_selector :css, "##{selector}"
+end
+
+Then("I should be redirected to the landing page") do
+  expect(current_path).to eq root_path
+end
+
+Then("show me the page") do
+  save_and_open_page
 end
