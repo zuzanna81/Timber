99a5dda5e452f8e0dd9faaa69ec4f753fba84b49
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2018-01-31 11:01:50 +0100

    Write feature and generate step defs

---
+++ features/step_definitions/upload_local_image_step.rb
@@ -0,0 +1,19 @@
+When("I visit photo album page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I see upload photo form") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I attach file {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I click on upload button") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see attached file {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
---
+++ features/user_can_upload_local_images.feature
@@ -0,0 +1,11 @@
+Feature: User can upload attached file
+  As a user
+  In order to upload photos to my photo album page
+  I would like to be able to attach a local file and upload it
+
+  Scenario: User can upload attached file
+    When I visit photo album page
+    Then I see upload photo form
+    And I attach file "Lisa.jpeg"
+    And I click on upload button
+    Then I should see attached file "Lisa.jpeg"
