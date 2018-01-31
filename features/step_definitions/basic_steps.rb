3c464d199fb312178532c306c33bbe09f00a3d6d
Zuzanna <zuzanna.bienkowska@gmail.com>
2018-01-31 15:22:29 +0100

    Add feature and step definitions for sign up feature Add devise gem

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -1,7 +1,28 @@
-When("I visit index page") do
+When("I visit the index page") do
   visit root_path
 end
 
 Then("I should see {string}") do |content|
   expect(page).to have_content content
+end
+
+Then("I click {string}") do |link|
+  click_link_or_button link
+end
+
+Then("I should be redirected to the {string} page") do |page_name|
+  expect(page.current_path).to eq page_path_from(page_name)
+end
+
+When("I fill in {string} with {string}") do |field, value|
+  fill_in field, with: value
+end
+
+def page_path_from(page_name)
+  case page_name.downcase
+    when 'sign up'
+      new_user_registration_path
+    else
+      root_path
+  end
 end
