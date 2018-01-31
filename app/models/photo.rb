01152c22a47fd8b9e780e9bf6fa232c024626df7
Francesco <francesco.falchi@gmail.com>
2018-01-31 19:00:34 +0100

    Add unit test for photo model, all green

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,6 @@ class Photo < ApplicationRecord
   has_attached_file :image
   validates_attachment :image,
                      content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+
+  validates_presence_of :title
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,7 +12,7 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
-  config.include Paperclip::Shoulda::
+  config.include Paperclip::Shoulda::Matchers
 end
 
 Shoulda::Matchers.configure do |config|
