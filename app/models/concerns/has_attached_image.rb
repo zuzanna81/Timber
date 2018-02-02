0aef02e40f3a9f7b397ad1959d21961aac66ca6a
Francesco <francesco.falchi@gmail.com>
2018-02-02 16:31:43 +0100

    Create env and store api keys, add bucket settings in hasattachedimage module

--- app/models/concerns/has_attached_image.rb
+++ app/models/concerns/has_attached_image.rb
@@ -3,6 +3,14 @@ module HasAttachedImage
 
   included do
     has_attached_file :image,
+                      s3_credentials: {
+                          bucket: ENV['AWS_BUCKET'],
+                          access_key_id: ENV['AWS_ACCESS_KEY'],
+                          secret_access_key: ENV['AWS_SECRET_KEY'],
+                          s3_region: ENV['AWS_REGION'],
+                          url: ':s3_domain_url',
+                          s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com"
+                      },
                       default_url: Rails.root + "spec/fixture/random.png"
 
     validates_attachment :image,
