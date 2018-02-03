c6c45fead17478904dc11e78be6d52e0b9947a30
lisa_eriksson <lisa.c.eriksson@gmail.com>
2018-02-03 10:23:00 +0100

    Deletes family tree migrations, controller and model

--- db/schema.rb
+++ db/schema.rb
@@ -15,16 +15,6 @@ ActiveRecord::Schema.define(version: 20180131145731) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "family_trees", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "relationship"
-    t.integer "date_of_birth"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "image"
-  end
-
   create_table "photos", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
