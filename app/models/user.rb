cb655a8d421941235e8b9ee44f82b99f5a00c686
Francesco <francesco.falchi@gmail.com>
2018-02-03 13:46:04 +0100

    Add invitable spec to schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
 
-  devise :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
