3347de0ea58c8d69b2da2696f445a7f9617d8a12
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2018-02-06 10:28:37 +0100

    Generate family controller and def create

---
+++ app/controllers/families_controller.rb
@@ -0,0 +1,17 @@
+class FamiliesController < ApplicationController
+
+	def new 
+		@family = Family.new 
+	end
+	
+	def create 
+		@family = Family.new(family_params)
+		@family.save 
+	end 	
+
+
+	private
+  	def family_params
+    	params.require(:family).permit(:name)
+  	end
+end
