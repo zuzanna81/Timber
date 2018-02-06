e89c930cc49ce2baae0df018b95bf2a71d51e423
Francesco <francesco.falchi@gmail.com>
2018-02-06 11:18:51 +0100

    Remove unnecessary sign up method

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,10 +3,6 @@ class RegistrationsController < Devise::RegistrationsController
 	private
 
 	def sign_up_params
-	  devise_parameter_sanitizer.sanitize(:sign_up)
-	end
-
-	def sign_up_params
 		params.require(:user).permit(:email, :password, :password_confirmation, :family)
 	end
 
