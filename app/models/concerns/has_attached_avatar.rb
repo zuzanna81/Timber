dd52c24a05b9a9f011ab90d7e72fae81bf9685a4
Francesco <francesco.falchi@gmail.com>
2018-02-07 16:51:29 +0100

    Insert user avatar in index Remove current password field from devise regitrations edit view Change to symbol in has attached avatar module

--- app/models/concerns/has_attached_avatar.rb
+++ app/models/concerns/has_attached_avatar.rb
@@ -11,7 +11,7 @@ module HasAttachedAvatar
                           access_key_id: ENV['AWS_ACCESS_KEY'],
                           secret_access_key: ENV['AWS_SECRET_KEY'],
                           s3_region: ENV['AWS_REGION'],
-                          url: ':s3_domain_url',
+                          url: :s3_domain_url,
                           s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com"
                       }
     validates_attachment :avatar,
