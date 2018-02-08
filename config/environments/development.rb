e7053eef123418af7b3a223d9ac351e035db2c46
Francesco <francesco.falchi@gmail.com>
2018-02-08 15:16:52 +0100

    Move smtp setting to env and refactor development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,12 +31,12 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'example.com',
+  address:              ENV['SMTP_ADDRESS'],
+  port:                 ENV['SMTP_PORT'],
+  domain:               ENV['SMTP_DOMAIN'],
   user_name:            ENV['GMAIL_USERNAME'],
   password:             ENV['GMAIL_PASSWORD'],
-  authentication:       'plain',
+  authentication:       ENV['SMTP_AUTH'],
   enable_starttls_auto: true  }
 
   config.action_mailer.perform_caching = false
