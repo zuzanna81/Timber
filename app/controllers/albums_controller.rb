a136d8a0790d658cef85de227dcf0daeef907cd6
Raoul DIFFOUO <diraulo@gmail.com>
2018-02-09 23:10:15 +0200

    Fix photo selection logic and layout

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -4,7 +4,7 @@ before_action :check_for_family, only: [:create]
 
   def index
     @albums = current_user.family.albums
-    @recent_photos = @albums.map { |a| a.photos }#.flatten.sort_by { |photo| photo.created_at }.last(3) 
+    @recent_photos = @albums.map(&:photos).flatten.sort_by(&:created_at).last(3)
   end
 
   def new
