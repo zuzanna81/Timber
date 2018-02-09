c279590bc630071ed9dc5dd1b253420d0c75923c
Francesco <francesco.falchi@gmail.com>
2018-02-10 00:38:39 +0100

    Refactor step definition in image and album step file

--- features/step_definitions/image_and_album_steps.rb
+++ features/step_definitions/image_and_album_steps.rb
@@ -46,7 +46,7 @@ Then('I should not see {string}') do |content|
   expect(page).not_to have_content content
 end
 
-Then(/^I should be on the album "([^"]*)" page$/) do |album_title|
+Then('I should be on the album {string} page') do |album_title|
   album = Album.find_by(title: album_title)
   expect(current_path).to eq album_path(album)
 end
