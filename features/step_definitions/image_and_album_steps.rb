d4989ba0233ad62ab341a6ed76840fe9fa7e0b83
Francesco <francesco.falchi@gmail.com>
2018-02-09 20:01:52 +0100

    Beautify all the steps

--- features/step_definitions/image_and_album_steps.rb
+++ features/step_definitions/image_and_album_steps.rb
@@ -1,46 +1,47 @@
-When("I visit new photo page") do
+When('I visit new photo page') do
   visit new_photo_path
 end
 
-When("I upload file {string}") do |file_name|
+When('I upload file {string}') do |file_name|
   attach_file('photo[image]', Rails.root + "spec/fixture/#{file_name}")
 end
 
-When("I upload the file {string}") do |file_name|
+When('I upload the file {string}') do |file_name|
   attach_file('user[avatar]', Rails.root + "spec/fixture/#{file_name}")
 end
 
-And("I should see {string} image") do |file_name|
+And('I should see {string} image') do |file_name|
   expect(page).to have_css("img[src*='#{file_name}']")
 end
 
-Then("I should not see {string} image") do |file_name|
+Then('I should not see {string} image') do |file_name|
   expect(page).not_to have_css("img[src*='#{file_name}']")
 end
 
-Given("the following album exists") do |table|
+Given('the following album exists') do |table|
   table.hashes.each do |title|
     create(:album, title)
   end
 end
 
-Given("the following image exist in album") do |table|
+Given('the following image exist in album') do |table|
   table.hashes.each do |image|
     family = Family.find_by(name: image[:family])
     album = create(:album, title: image[:album_title], family: family)
-    create(:photo, image.except!("album_title", "family").merge!(album: album))
+    create(:photo, image.except!('album_title', 'family').merge!(album: album))
   end
 end
 
-Then("the last album should have title {string}") do |album_title|
+Then('the last album should have title {string}') do |album_title|
   album = Album.last
   expect(album.title).to eq album_title
 end
 
-Then("the last album should belong to {string} family") do |family_name|
+Then('the last album should belong to {string} family') do |family_name|
   family = Family.find_by(name: family_name)
-  expect(Family.last.name).to eq family.name end
+  expect(Family.last.name).to eq family.name
+end
 
-Then("I should not see {string}") do |content|
-  expect(page).not_to have_content content 
+Then('I should not see {string}') do |content|
+  expect(page).not_to have_content content
 end
