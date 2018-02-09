96955af4c6295981aad816683adb9ccbb0d3a01a
Francesco <francesco.falchi@gmail.com>
2018-02-09 10:26:10 +0100

    Add unit tests for user first name Add required validation for first name in user model and add first name to user factory Run migrate to update schema

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :user do
     email "MyEmail@hotmail.com"
+    first_name "Daphne"
     password "MyPassword"
     password_confirmation "MyPassword"
-    family 
+    family
   end
 end
